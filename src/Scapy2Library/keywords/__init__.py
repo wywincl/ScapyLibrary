1e5fb654e0ec12140d766edd25bd68135d55af9d
John.Wang <m18665302460_2@163.com>
2015-04-26 14:16:21 +0800

    Update __init__.py
    添加关键字类文件

---
+++ src/Scapy2Library/keywords/__init__.py
@@ -0,0 +1,8 @@
+from _corekeywords import _ScapyKeywords
+from _runonfailure import _RunOnFailureKeywords
+from _logging import _LoggingKeywords
+
+
+__all__ = ["_ScapyKeywords",
+           "_RunOnFailureKeywords",
+           "_LoggingKeywords"]
