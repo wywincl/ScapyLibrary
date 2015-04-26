88eb3b36fedfafd6d46fd06fefdf57a46f2e2b5c
John.Wang <m18665302460_2@163.com>
2015-04-26 14:10:30 +0800

    Create __init__.py
    添加初始化文件

---
+++ src/Scapy2Library/utils/__init__.py
@@ -0,0 +1,9 @@
+from _scapy_send import *
+from _global import *
+
+__all__ = ["send_igmp_pkt",
+           "send_ip_pkt",
+           "send_packet",
+           "generate_udp_pkt",
+           "generate_tcp_pkt",
+           "capture_packet"]
